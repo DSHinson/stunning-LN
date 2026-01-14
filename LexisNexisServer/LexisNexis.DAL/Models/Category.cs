f1f8df97c8121fe7d5676e77aa998cadc327767b
DSHinson <DSHinson@outlook.com>
2026-01-14 22:08:13 +0200

    Added model for product and Category

---
+++ LexisNexisServer/LexisNexis.DAL/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using LexisNexis.DAL.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexisNexis.DAL.Models
+{
+    record Category : EntityBase
+    {
+        public string Name { get; set; } = "";
+        public int? ParentId { get; set; }
+    }
+}
---
+++ LexisNexisServer/LexisNexis.DAL/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using LexisNexis.DAL.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexisNexis.DAL.Models
+{
+    record Product : EntityBase
+    {
+        public required string Name { get; set; } = "";
+        public int CategoryId { get; set; }
+    }
+}
