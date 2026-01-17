fdd44704f7bc1dddde6e775e928edefdb4accbb1
DSHinson <DSHinson@outlook.com>
2026-01-17 17:44:12 +0200

    Init client - scaffold pages and components

---
+++ LexisNexisFrontEnd/LexisNexis.Client/src/app/pages/categories/categories.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  imports: [],
+  templateUrl: './categories.html',
+  styleUrl: './categories.css',
+  standalone: true
+})
+export class Categories {
+
+}
