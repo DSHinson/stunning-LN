9226d3dcb399f1d1f9390b84744c96ae95ba9946
DSHinson <DSHinson@outlook.com>
2026-01-17 16:42:59 +0200

    Added simple event logging

--- LexisNexisServer/LexisNexis.Common/CQRS/EventPlayerService.cs
+++ LexisNexisServer/LexisNexis.Common/CQRS/EventPlayerService.cs
@@ -1,11 +1,11 @@
 ﻿using LexisNexis.Common.CQRS.Command;
 using LexisNexis.Common.CQRS.Query;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LexisNexis.Common.CQRS
@@ -16,60 +16,85 @@ namespace LexisNexis.Common.CQRS
         (
             Type CommandType,
             Type ResultType,
-            IEvent Command
+            IEvent Command,
+            DateTime Timestamp
         );
 
         private readonly IServiceProvider _serviceProvider;
         private readonly List<ReplayEntry> _eventLog = new();
+        private readonly ILogger<EventPlayerService> _logger;
 
-        public EventPlayerService(IServiceProvider serviceProvider)
+        public EventPlayerService(IServiceProvider serviceProvider, ILogger<EventPlayerService> logger)
         {
             _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<TResult> EmitAsync<TResult>(IQuery<TResult> query)
         {
+            _logger.LogInformation("Emitting query {QueryType} at {Timestamp}", query.GetType().Name, DateTime.UtcNow);
+
             using (IServiceScope scope = _serviceProvider.CreateScope())
             {
                 var queryDispatcher = scope.ServiceProvider.GetRequiredService<IQueryDispatcher>();
-                return await queryDispatcher.DispatchAsync(query);
+                TResult result = await queryDispatcher.DispatchAsync(query);
+
+                _logger.LogInformation("Query {QueryType} completed at {Timestamp}", query.GetType().Name, DateTime.UtcNow);
+
+                return result;
             }
         }
 
         public async Task<TResult> EmitAsync<TResult>(ICommand<TResult> command)
         {
-            _eventLog.Add(new ReplayEntry(CommandType: command.GetType(), ResultType: typeof(TResult), Command: command));
+            var timestamp = DateTime.UtcNow;
+            _eventLog.Add(new ReplayEntry(command.GetType(), typeof(TResult), command, timestamp));
+
+            _logger.LogInformation("Emitting command {CommandType} at {Timestamp}", command.GetType().Name, timestamp);
 
             using (IServiceScope scope = _serviceProvider.CreateScope())
             {
                 var commandDispatcher = scope.ServiceProvider.GetRequiredService<ICommandDispatcher>();
-                return await commandDispatcher.DispatchAsync(command);
+                TResult result = await commandDispatcher.DispatchAsync(command);
+
+                _logger.LogInformation("Command {CommandType} completed at {Timestamp}", command.GetType().Name, DateTime.UtcNow);
+
+                return result;
             }
         }
 
         public async Task ReplayAsync()
         {
-            using var scope = _serviceProvider.CreateScope();
-            var serviceProvider = scope.ServiceProvider;
+            _logger.LogInformation("Starting replay of {Count} events at {Timestamp}", _eventLog.Count, DateTime.UtcNow);
 
-            // Resolve the dispatcher from DI
-            ICommandDispatcher dispatcher = serviceProvider.GetRequiredService<ICommandDispatcher>();
+            using var scope = _serviceProvider.CreateScope();
+            var dispatcher = scope.ServiceProvider.GetRequiredService<ICommandDispatcher>();
 
             foreach (ReplayEntry entry in _eventLog)
             {
-                EventReplayBehaviorAttribute? attr = entry.Command.GetType().GetCustomAttributes(typeof(EventReplayBehaviorAttribute), false).FirstOrDefault() as EventReplayBehaviorAttribute;
+                EventReplayBehaviorAttribute? attr = entry.Command.GetType()
+                    .GetCustomAttributes(typeof(EventReplayBehaviorAttribute), false)
+                    .FirstOrDefault() as EventReplayBehaviorAttribute;
 
                 if (attr == null || !attr.Options.HasFlag(EventReplayOptions.Replayable))
                 {
                     continue;
                 }
 
-                MethodInfo method = typeof(ICommandDispatcher).GetMethod(nameof(ICommandDispatcher.DispatchAsync))!.MakeGenericMethod(entry.ResultType);
+                _logger.LogInformation("Replaying command {CommandType} originally emitted at {Timestamp}",
+                    entry.CommandType.Name, entry.Timestamp);
+
+                MethodInfo method = typeof(ICommandDispatcher).GetMethod(nameof(ICommandDispatcher.DispatchAsync))!
+                    .MakeGenericMethod(entry.ResultType);
 
                 var task = (Task)method.Invoke(dispatcher, new object[] { entry.Command })!;
                 await task.ConfigureAwait(false);
 
+                _logger.LogInformation("Finished replaying command {CommandType} at {Timestamp}",
+                    entry.CommandType.Name, DateTime.UtcNow);
             }
+
+            _logger.LogInformation("Replay finished at {Timestamp}", DateTime.UtcNow);
         }
     }
 }
